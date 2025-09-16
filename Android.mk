b474f0dd84d3b4d4ea13807781cc98dec9c2bcc8
hiroshi <hiroshi@tanuki.page>
2025-09-15 20:42:43 -0400

    tiro: Setup firmware extraction

--- Android.mk
+++ Android.mk
@@ -3,3 +3,29 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),tiro)
+
+$(call add-radio-file-sha1-checked,radio/aop.img,00d38669a0ebd6882680b9b66c11eed494fb5b51)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,1b81413c835a4f0a9023ea8047cb7cdc9472fb37)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,7201be06a50c8eb95cc2105100accc2822254b8d)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,c523305fa677e9a57f480c68acf4c825ac047dea)
+$(call add-radio-file-sha1-checked,radio/cpucp_dtb.img,8159b1550227ba4562b96af4e740561ce94e6cd7)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,18011ce046be97c4b7fac9377c555242488e2ad8)
+$(call add-radio-file-sha1-checked,radio/dsp.img,91942d951825ca0c4ce1eec4bef5982ff4316d55)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,298d306ef817fa5143b42ca4b88a6542a48e54fa)
+$(call add-radio-file-sha1-checked,radio/hyp.img,ff5f453defa6ff98edcb0d3c76d585ba9d700131)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,55f67ed462c39adc3be492d8a36093cc49bb0cd7)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,bcb4e8d8f329e1793950a2f483ae13426348af7a)
+$(call add-radio-file-sha1-checked,radio/modem.img,0a74bb127f1e931baece638b7cd7aadc0c09b6e4)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,0f3d46672873f2ed1320f7d0ed312b098b1eb33a)
+$(call add-radio-file-sha1-checked,radio/shrm.img,e9ec9b2570ce31d871d226315bea181d21d4f140)
+$(call add-radio-file-sha1-checked,radio/spuservice.img,d43828bb730984bf3c56fe2f5699459f61c64426)
+$(call add-radio-file-sha1-checked,radio/tz.img,1882d1731b76323b09396f3bbbefe72427c3b80d)
+$(call add-radio-file-sha1-checked,radio/uefi.img,d3349f19f203ced10fb60110db2d076c0d64c9aa)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,9fb332c2a0f4d6a0ba876ec2f19bc480af21ab1a)
+$(call add-radio-file-sha1-checked,radio/xbl.img,8cfd262818aa83fb70c1a3448573c7a9fa2a2301)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,df6781f8553ab249b5ef400ea829ab0cf6645cff)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,a7495798dec498c6b221bbd11fcfdca014e72cd3)
+
+endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,3 +1,26 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
+
+AB_OTA_PARTITIONS += \
+    aop \
+    aop_config \
+    bluetooth \
+    cpucp \
+    cpucp_dtb \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    shrm \
+    spuservice \
+    tz \
+    uefi \
+    uefisecapp \
+    xbl \
+    xbl_config \
+    xbl_ramdump
